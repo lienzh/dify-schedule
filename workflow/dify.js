0da2f2cfdb88a8d7f6fdbe3a5825740df8ede129
lienzh <zjtzjjlan@gmail.com>
2025-02-04 12:28:08 +0800

    Update dify.js

--- workflow/dify.js
+++ workflow/dify.js
@@ -61,7 +61,7 @@ async function run(args) {
 
     const message = messageList.join(`\n${"-".repeat(15)}\n`);
     Notify.pushMessage({
-      title: "Dify工作流定时助手",
+      title: "Lien的悬置女境",
       content: message,
       msgtype: "text"
     });
